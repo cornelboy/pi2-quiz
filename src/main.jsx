e707c9f7a44c38f1d39251752d742be865e52e03
cornelboy <134008021+cornelboy@users.noreply.github.com>
2026-03-01 16:10:33 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Pi2Quiz from './Pi2Quiz'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Pi2Quiz />
+  </React.StrictMode>
+)
